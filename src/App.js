2c08c6a471b6c6afc8272a4018dd5ed44fafe955
thelittleblacksmith <einers@outlook.com>
2017-03-29 22:18:12 -0700

    omg it works 🎉✨

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 //External Libraries
-// import Marked from 'marked';
+import marked from 'marked';
 //React
 import React, { Component } from 'react';
 import './App.css';
-// Import Components
-import Input from './components/input';
-import Output from './components/output';
+
+function markdownToHTML (md){
+  return {__html:marked(md)};
+}
+
+const Output = (props) => {
+  return <div dangerouslySetInnerHTML={ markdownToHTML(props.value) } />
+  // return <div>{props.value}</div>;
+};
+
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      value:'Hello\n========\nIDK markdown! Wow!!', 
-      outputValue: ''
+      value:'Hello\n========\nIDK markdown! Wow!!'
     };
   }
 
@@ -21,10 +27,20 @@ class App extends Component {
       <div className="App">
         <h1>React Markdown Previewer</h1>
         <div className="container">
-          <Input />
-          <Output value={this.state.outputValue}/>
+         
+          <textarea name="" id="" cols="30" rows="10"
+            value={this.state.value}
+            onChange={ (e) => { this.setState({ value: e.target.value }) } }
+          ></textarea>
+         
+          <div className="output">
+            <h2 className="output_title">Output</h2>
+            <Output value={marked(this.state.value)}/>
+            {/*<div> {this.state.value} </div>*/}
+         </div>
+
         </div>        
-      </div>
+      </div> 
     );
   }
 }
